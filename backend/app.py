fbdadf85a2388407dbd77b453c565e241f1e0761
Sidra Babar <140131133+Sidra001Babar@users.noreply.github.com>
2026-01-28 10:49:19 +0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -31,7 +31,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'sidrab349@gmail.com'
-app.config['MAIL_PASSWORD'] = 'nmzy cqdc qkma axbc'
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_DEFAULT_SENDER'] = 'sidrab349@gmail.com'
 
 # Init extensions
