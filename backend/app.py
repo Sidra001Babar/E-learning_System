26ab43a86d393dc95cca69bf7c25b06106bb10fa
Sidra Babar <sidrab349@gmail.com>
2026-01-31 11:35:39 +0500

    delete caches

--- backend/app.py
+++ backend/app.py
@@ -42,6 +42,6 @@ migrate.init_app(app, db)
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    # app.run(debug=True)
+    app.run(debug=True)
     port = int(os.getenv("PORT", 5000))  # Railway provides PORT automatically
     app.run(host="0.0.0.0", port=port, debug=False)
